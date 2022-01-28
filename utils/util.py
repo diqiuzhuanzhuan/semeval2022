c2cd243c457781c96112767a0b491163d954d8dd
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-01-28 11:16:15 +0800

    fix kfold bug

--- utils/util.py
+++ utils/util.py
@@ -464,19 +464,19 @@ def k_fold(train_file, dev_file, k=10, recreate=False):
         output_dev_file = "{}_{}".format(dev_file, index)
         if os.path.exists(output_train_file) and os.path.exists(output_dev_file) and not recreate:
             print("k fold files are exsited! so we don't recreate them")
-            continue
-        with open(output_train_file, "w") as f:
-            for i in train:
-                for fields in zip(*all_fields[i]):
-                    f.write(" ".join(fields))
+        else:
+            with open(output_train_file, "w") as f:
+                for i in train:
+                    for fields in zip(*all_fields[i]):
+                        f.write(" ".join(fields))
+                        f.write("\n")
                     f.write("\n")
-                f.write("\n")
-        with open(output_dev_file, "w") as f:
-            for i in dev:
-                for fields in zip(*all_fields[i]):
-                    f.write(" ".join(fields))
+            with open(output_dev_file, "w") as f:
+                for i in dev:
+                    for fields in zip(*all_fields[i]):
+                        f.write(" ".join(fields))
+                        f.write("\n")
                     f.write("\n")
-                f.write("\n")
         output_files.append((output_train_file, output_dev_file))
         index += 1
     return output_files
